2f033e115f72a9a26977878e863e8ce1cf7a9c3f
Reece Crook <reececrook16@gmail.com>
2023-07-31 22:47:46 -0600

    Completed Lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if i == 0:
+            i -= 1
+            print("Happy New Year!")
+        else:
+            print(i)
+            i -= 1
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [number * number for number in int_list]
+    return squared_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(101):
+        if i == 0:
+            pass
+        elif (i / 3).is_integer() and (i / 5).is_integer():
+            print("FizzBuzz")
+        elif (i / 3).is_integer():
+            print("Fizz")
+        elif (i / 5).is_integer():
+            print("Buzz")
+        else:
+            print(i)
